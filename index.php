d4d432cf64f8a03cd63463bf3e7c8ab1fe74f217
sfmolx <42357096+sfmolx@users.noreply.github.com>
2018-08-24 10:48:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ echo "<h1>Openshift Workshop v2.0</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 echo "<br><hr>";
 echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
+
+phpinfo ();
+
+/*
 $conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -16,4 +20,6 @@ if ($result->num_rows > 0) {
     echo "0 results";
 }
 $conn->close();
+
+*/
 ?>
