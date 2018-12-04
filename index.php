d79932225c16969fa20f92d484cc5c7c30c9b65e
sfmolx <42357096+sfmolx@users.noreply.github.com>
2018-12-04 10:12:49 -0200

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
 
 phpinfo ();
 
+/*
 $conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -19,4 +20,5 @@ if ($result->num_rows > 0) {
     echo "0 results";
 }
 $conn->close();
+*/
 
