5cdda311238e1c0219892825c5601e4dfa5f9a64
sfmolx <42357096+sfmolx@users.noreply.github.com>
2018-12-05 15:04:51 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo "<h1>Openshift Workshop v2.0</h1> ";
 echo $_SERVER['SERVER_ADDR'];
-echo "<br>x<hr>";
+echo "<br>x<br>x<br>x<br>x<hr>";
 //echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
 
 phpinfo ();
